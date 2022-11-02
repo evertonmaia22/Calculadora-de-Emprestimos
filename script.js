36de3156d6530d387094df51e9edc8f7d9d66ed3
maia.everton22@gmail.com <maia.everton22@gmail.com>
2022-11-02 17:21:36 -0300

    Validação de CNPJ implementada

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
+// Função para validar CNPJ
+
+const span = document.querySelector('#span-required');
+const input = document.querySelector("#input");
+
+function CnpjValidade() {
+
+    if (input.value != '11.111.111/1111-11') {
+        span.innerHTML = 'CNPJ não aprovado para empréstimo';
+        span.style.color = 'red';
+
+    }
+    else {
+        document.querySelector('#span-required').innerHTML = 'Empréstimo aprovado para este CNPJ';
+        span.style.color = 'green';
+    }
+    if (input.value < 14) {
+        span.innerHTML = 'Digite no minímo 14 números';
+        span.style.color = 'red'
+    }
+}
 
 // Funcionalidade para incrementar caracteres especiais durante a digitação do CNPJ
 
-const input = document.querySelector("#input");
+// Se o tamanho do input for igual a 2 ou a 6 irá adicionar '.' 
+// Senao se o tamanho do input for igual a 10 irá adicionar '/'
+// Senao se o tamanho do input for igual a 15 irá adicionar '-'
 
 input.addEventListener('keypress', () => {
-    
-    
-    // Se o tamanho do input for igual a 2 ou a 6 irá adicionar '.' 
-    // Senao se o tamanho do input for igual a 10 irá adicionar '/'
-    // Senao se o tamanho do input for igual a 15 irá adicionar '-'
 
     let inputLength = input.value.length
+
     if (inputLength === 2 || inputLength === 6) {
         input.value += '.';
     } else if (inputLength === 10) {
         input.value += '/';
     } else if (inputLength === 15) {
         input.value += '-';
-    } 
+    }
 })
 
-    // Função para calcular
+// Função para calcular
+// Manipulando o DOM 
+// Utilizando querySelector para retornar o primeiro elemento dentro do documento
+// Condicionais (if) adicionadas para que o usuário digite algum valor no input
 
 function Calculate() {
 
-    // Manipulando o DOM 
-    // Utilizando querySelector para retornar o primeiro elemento dentro do documento
-    // Condicionais (if) adicionadas para que o usuário digite algum valor no input
-
     const amount = document.querySelector("#amount").value;
 
-    if(amount === ''){
+    if (amount === '') {
         return alert('Digite o valor do Empréstimo')
     };
 
     const rate = document.querySelector("#interest").value;
 
-    if(rate === ''){
+    if (rate === '') {
         return alert('Digite os juros ao mês')
     };
-    
+
     const months = document.querySelector("#months").value;
 
-    if(months === ''){
+    if (months === '') {
         return alert('Digite a quantidade de meses')
     };
-    
-    // Valor do juros / 100
-    const interest = rate/100
 
     // Calculando juros compostos
+    // Valor do juros / 100
     // O valor do juros está fixo em 3% no código html.
-    // Utilizando o método Math.pow() eu consigo retornar um número que representa a base fornecida.
-    // (1 + Valor do juros) * Quantidade de meses.
-  
+    // Utilizando o método Math.pow() retorna um número que representa a base fornecida.
+
+    const interest = rate / 100
+
     const total = amount * Math.pow((1 + interest), months);
 
     // Calculando o valor total
-    // Valor total dividido pela quantidade de meses.
 
     const valueMonth = ((total / months));
 
@@ -65,11 +81,12 @@ function Calculate() {
     // Utilizando querySelector para retornar o primeiro elemento dentro do documento.
     // Retornando o conteúdo através do método innerHtml.
     // Extraindo o valor mensal e valor total.
-    
-    document.querySelector("#total").innerHTML = "O valor do empréstimo para este CNPJ ficará por mês: R$" + valueMonth.toFixed(2);
 
-    document.querySelector("#valueTotal").innerHTML = "E o valor total ficará em: R$" + total.toFixed(2);
+
+    document.querySelector("#total").innerHTML = "Valor da Mensalidade: R$" + valueMonth.toFixed(2);
+
+    document.querySelector("#valueTotal").innerHTML = "Valor Total: R$" + total.toFixed(2);
 
 }
 
- 
+
